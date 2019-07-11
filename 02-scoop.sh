89f49387d9405ade69743e9e22b16f3ff6357205
Ivan De Marino <detronizator@gmail.com>
2019-07-11 22:27:38 +0200

    Stronger check for windows-specific "scoop" setup

--- 02-scoop.sh
+++ 02-scoop.sh
@@ -1,6 +1,6 @@
 # This is for Windows only, where you should install scoop!
 # See: https://scoop.sh/
 
-if which powershell &>/dev/null; then
+if [[ "WINDOWS" == ${OSNAME} ]] && which powershell &>/dev/null; then
 	function scoop() { powershell -ex unrestricted scoop.ps1 "$@" ;} && export -f scoop
 fi
