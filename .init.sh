c4785f160a6dce7f7c237c284fb5a7b761e03861
Ivan De Marino <idemarino@newrelic.com>
2020-04-07 15:19:36 +0200

    Adding JDK switching on macOS

--- .init.sh
+++ .init.sh
@@ -14,3 +14,23 @@ esac
 for BASHRC_D_FILE in `ls ${THIS_DIR}/*.sh`; do
     source $BASHRC_D_FILE
 done
+
+# Colorful logging helper: INFO (green) level
+function info() {
+	echo -e "\e[32m*\e[39m ${*}"
+}
+
+# Colorful logging helper: WARN (orange) level
+function warn() {
+	echo -e "\e[33m*\e[39m ${*}"
+}
+
+# Colorful logging helper: ERROR (red) level
+function error() {
+	echo -e "\e[31m*\e[39m ${*}"
+}
+
+# Colorful logging helper: just a new line
+function nln() {
+	echo ""
+}
---
+++ 10-jdk.sh
@@ -0,0 +1,28 @@
+if [[ "MACOSX" == ${OSNAME} ]]; then
+	MACOS_JAVAHOME_BIN="/usr/libexec/java_home"
+
+	DEFAULT_JAVA_HOME=$(${MACOS_JAVAHOME_BIN} 2>/dev/null)
+	export JAVA_HOME="${DEFAULT_JAVA_HOME}"
+
+	function jdkset() {
+		local ver="${1}"
+
+		FOUND_JAVA_HOME=$(${MACOS_JAVAHOME_BIN} -v ${ver} 2>/dev/null)
+		if [[ ${FOUND_JAVA_HOME} == ${DEFAULT_JAVA_HOME} ]]; then
+			warn "Setting JDK: ${FOUND_JAVA_HOME} (default)"
+		else
+			info "Setting JDK: ${FOUND_JAVA_HOME}"
+		fi
+
+		export JAVA_HOME="${FOUND_JAVA_HOME}"
+		nln
+
+		jdkver
+	}
+
+	function jdkver() {
+		info "JDK version:"
+		java -version
+		nln
+	}
+fi
