7edf56b376a585c1569b517dba9a717f60467dad
Ivan De Marino <idemarino@newrelic.com>
2021-02-02 12:40:31 +0100

    Adapt jdkset/jdkver scripts to Big Sur

--- 10-jdk.sh
+++ 10-jdk.sh
@@ -7,6 +7,10 @@ if [[ "MACOSX" == ${OSNAME} ]]; then
 	function jdkset() {
 		local ver="${1}"
 
+		# Unset `JAVA_HOME` before using the `java_home` command
+		unset JAVA_HOME
+
+		# Print a warning if the version requested wasn't found and it defaulted
 		FOUND_JAVA_HOME=$(${MACOS_JAVAHOME_BIN} -v ${ver} 2>/dev/null)
 		if [[ ${FOUND_JAVA_HOME} == ${DEFAULT_JAVA_HOME} ]]; then
 			warn "Setting JDK: ${FOUND_JAVA_HOME} (default)"
