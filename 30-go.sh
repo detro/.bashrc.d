c9b9e8787199b7ae209c1a25363ada6340e369af
Ivan De Marino <idemarino@newrelic.com>
2021-02-02 12:40:52 +0100

    Tweaks to the go-lang script

--- 30-go.sh
+++ 30-go.sh
@@ -1,6 +1,4 @@
-# Go (see https://golang.org/doc/install)
-#
-# Assumes this layout for the Go installation:
+# Suggested layout for the Go installation (if using packages from https://golang.org/doc/install):
 # ~/.go
 #   ├── <VERSION>
 #   │   ├── go                      <--- GOROOT
@@ -10,12 +8,20 @@
 #   └── go                          <--- GOPATH
 #
 if [ -d "$HOME/.go/current/go/bin" ]; then
-	# Go installed under "~/.go" (Windows, Linux)
+    # If Go is installed in the home directory (as shown above)
     export GOROOT="$HOME/.go/current/go"
     export GOPATH="$HOME/Workspaces/go"
+    export GO111MODULE=on
     export PATH="$GOROOT/bin:$GOPATH/bin:$PATH"
 elif which go &> /dev/null; then
-	# Go installed on the system level (macOS)
-	export GOPATH="$HOME/Workspaces/go"
-	export PATH="$GOROOT/bin:$GOPATH/bin:$PATH"
+    # If Go is installed on system level
+
+    if [[ -x "$(which brew)" ]] && [[ -d "$(brew --prefix)/opt/go/libexec" ]]; then
+        # Only on Mac with Homebrew
+        export GOROOT="$(brew --prefix)/opt/go/libexec"
+    fi
+
+    export GOPATH="$HOME/Workspaces/go"
+    export GO111MODULE=on
+    export PATH="$GOROOT/bin:$GOPATH/bin:$PATH"
 fi
