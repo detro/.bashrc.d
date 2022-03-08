325232b8a8530a1f54ef5e27f8a1913cc8f0e9b9
Ivan De Marino <ivan.de.marino@gmail.com>
2022-03-08 15:56:16 +0000

    Adding support to have 'goenv' under '/usr/local/opt/'

--- 30-golang-goenv.sh
+++ 30-golang-goenv.sh
@@ -1,7 +1,19 @@
 # Based on: https://github.com/syndbg/goenv/blob/master/INSTALL.md
-if [[ -x "$(which goenv)" ]] && [[ -d "${HOME}/.goenv" ]]; then
-	# Define environment variable GOENV_ROOT to point to the path where goenv repo is cloned
+#
+# The only thing you need for this to work, is to git-clone according to the INSTALL
+# readme. Then, install the Go version(s) you need.
+
+# Define environment variable GOENV_ROOT to point to the path where goenv repo is cloned
+if [[ -d "${HOME}/.goenv" ]]; then
+	# Found at `~/.goenv`
 	export GOENV_ROOT="${HOME}/.goenv"
+	# Found at `/usr/local/opt/goenv`
+elif [[ -d "/usr/local/opt/goenv" ]]; then
+	export GOENV_ROOT="/usr/local/opt/goenv"
+fi
+
+# If `GOENV_ROOT` was found and defined, initialize goenv
+if [[ -n "${GOENV_ROOT}" ]]; then
 	export PATH="${GOENV_ROOT}/bin:$PATH"
 
 	# Add goenv init to your shell to enable shims, management of GOPATH and GOROOT and auto-completion
