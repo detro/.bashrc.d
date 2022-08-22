5c1c23c84bcea1c64de08bf1569401ee1e2424c7
Ivan De Marino <detronizator@gmail.com>
2022-08-22 12:40:10 +0100

    Making homebrew work on both macOS INTEL and ARM

--- 10-homebrew.sh
+++ 10-homebrew.sh
@@ -1,5 +1,14 @@
-BREW_HOME="/opt/homebrew"
-BREW_BIN="${BREW_HOME}/bin/brew"
+# Usually, on Mac Intel
+if [[ -x "/usr/local/bin/brew" ]]; then
+	export BREW_HOME="/usr/local"
+fi
+
+# Usually, on Mac ARM
+if [[ -x "/opt/homebrew/bin/brew" ]]; then
+	export BREW_HOME="/opt/homebrew"
+fi
+
+export BREW_BIN="${BREW_HOME}/bin/brew"
 
 # Setup shell for Homebrew
 if [[ -x "${BREW_BIN}" ]]; then
