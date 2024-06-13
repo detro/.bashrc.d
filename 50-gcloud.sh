e6fa4ff530ce5eaf019237fe86ed7f1ac764bb9f
Ivan De Marino <ivan.demarino@snyk.io>
2024-06-13 09:55:49 +0100

    Revamp how gcloud works

--- 50-gcloud.sh
+++ 50-gcloud.sh
@@ -1,6 +1,14 @@
-# Depends on `google-cloud-sdk`.
-#
-# See: https://formulae.brew.sh/cask/google-cloud-sdk.
-if [[ -d "$(brew --prefix)/share/google-cloud-sdk" ]]; then
-	source "$(brew --prefix)/share/google-cloud-sdk/path.bash.inc"
+# First, install it following: https://cloud.google.com/sdk/docs/install-sdk.
+# After uncompressing the downloaded `.tar.gz`, place it in a location that will become it's "installation path".
+# Set the following GCLOUD_INSTALL_PATH to that directory:
+GCLOUD_INSTALL_PATH=${HOME}/bin/google-cloud-sdk
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f "${GCLOUD_INSTALL_PATH}/path.bash.inc" ]; then
+	source "${GCLOUD_INSTALL_PATH}/path.bash.inc"
+fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f "${GCLOUD_INSTALL_PATH}/completion.bash.inc" ]; then
+	source "${GCLOUD_INSTALL_PATH}/completion.bash.inc"
 fi
