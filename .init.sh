3bb0034fd4735c58c86f0bd3b98a86502709ec43
Ivan De Marino <ivan.demarino@snyk.io>
2024-06-13 09:55:56 +0100

    tweak `info` messages

--- .init.sh
+++ .init.sh
@@ -17,17 +17,17 @@ done
 
 # Colorful logging helper: INFO (green) level
 function info() {
-	echo -e "\e[32m*\e[39m ${*}"
+	echo -e "\e[32m* ${*}\e[39m"
 }
 
 # Colorful logging helper: WARN (orange) level
 function warn() {
-	echo -e "\e[33m*\e[39m ${*}"
+	echo -e "\e[33m* ${*}\e[39m"
 }
 
 # Colorful logging helper: ERROR (red) level
 function error() {
-	echo -e "\e[31m*\e[39m ${*}"
+	echo -e "\e[31m* ${*}\e[39m"
 }
 
 # Colorful logging helper: just a new line
