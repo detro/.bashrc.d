c2628c04a18adcc46bb635725c413a6f13acf105
Ivan De Marino <ivan.de.marino@gmail.com>
2025-04-11 07:48:56 +0100

    updated support for ASDF to support both bash and homebrew isntalls

--- 11-asdf.sh
+++ 11-asdf.sh
@@ -1,8 +1,12 @@
-# ASDF installed with the "Bash & Git" method
+# ASDF installed with the "Bash & Git" method (pre-0.16)
 # See: https://asdf-vm.com/guide/getting-started.html#_3-install-asdf
-
 if [[ -e "$HOME/.asdf/asdf.sh" ]]; then
 	. $HOME/.asdf/asdf.sh
 	. $HOME/.asdf/completions/asdf.bash
 fi
 
+## ASDF installed via Homebrew (0.16 and later)
+if [[ -e "$(brew --prefix)/opt/asdf/libexec/asdf.sh" ]]; then
+	. /opt/homebrew/opt/asdf/libexec/asdf.sh
+	. /opt/homebrew/etc/bash_completion.d/asdf
+fi
