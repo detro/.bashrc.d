620a437edc6b7f4edbe4e0f583b591b7061190c8
Ivan De Marino <ivan.demarino@snyk.io>
2026-03-23 08:19:19 +0000

    feat(kubernetes): option to use `nvim` as editor

--- 30-kubernetes.sh
+++ 30-kubernetes.sh
@@ -13,6 +13,8 @@ complete -F __start_kubectl k
 # Editor for `kubectl edit`
 if which subl &>/dev/null; then
     export KUBE_EDITOR="subl -w"
+elif which nvim &>/dev/null; then
+    export KUBE_EDITOR="nvim"
 elif which vim &>/dev/null; then
     export KUBE_EDITOR="vim"
 fi
