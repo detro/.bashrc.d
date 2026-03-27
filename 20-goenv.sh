9a8de22aaf11afe3c9365f998e4ca1f5dc018918
Ivan De Marino <ivan.demarino@snyk.io>
2026-03-27 16:22:46 +0000

    Removing goenv and cleanup

--- 20-goenv.sh
+++
@@ -1,7 +0,0 @@
-if [[ -x "$(which goenv)" ]]; then
-	export GOENV_ROOT="$HOME/.goenv"
-	export PATH="$GOENV_ROOT/bin:$PATH"
-	eval "$(goenv init -)"
-	export PATH="$GOROOT/bin:$PATH"
-	export PATH="$PATH:$GOPATH/bin"
-fi
