87b9e88a9230940a756f6cfdf6b0fbc214731089
Ivan De Marino <ivan.de.marino@gmail.com>
2026-05-01 10:05:09 +0100

    feat: revamp asdf setup

--- 11-asdf.sh
+++ 11-asdf.sh
@@ -1,20 +1,16 @@
-# ASDF installed with the "Bash & Git" method (pre-0.16)
-# See: https://asdf-vm.com/guide/getting-started.html#_3-install-asdf
-if [[ -e "$HOME/.asdf/asdf.sh" ]]; then
-	info "⌨️ ASDF detected (via Bash & Git)"
-	. $HOME/.asdf/asdf.sh
-	. $HOME/.asdf/completions/asdf.bash
-fi
+if which asdf &>/dev/null; then
+	# Add shims to PATH
+	if [[ -d "${HOME}/.asdf/shims" ]]; then
+		info "⌨️ ASDF setup PATH"
+		export PATH="${HOME}/.asdf/shims:$PATH"
+	fi
 
-# ASDF installed via Homebrew (0.16 and later)
-if [[ -e "$(brew --prefix)/opt/asdf/libexec/asdf.sh" ]]; then
-	info "⌨️ ASDF detected (via homebrew)"
-	. /opt/homebrew/opt/asdf/libexec/asdf.sh
-	. /opt/homebrew/etc/bash_completion.d/asdf
-fi
+	info "⌨️ ASDF setup Bash Completion"
+	. <(asdf completion bash)
 
-# Handle Java Plugin
-if [[ -e "~/.asdf/plugins/java/set-java-home.bash" ]]; then
-	info "⌨️ ASDF configured Java Plugin"
-	. ${HOME}/.asdf/plugins/java/set-java-home.bash
+	# Handle Java Plugin
+	if [[ -e "${HOME}/.asdf/plugins/java/set-java-home.bash" ]]; then
+		info "⌨️ ASDF configure Java Plugin"
+		. ${HOME}/.asdf/plugins/java/set-java-home.bash
+	fi
 fi
