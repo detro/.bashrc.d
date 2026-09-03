61622600b7ef815626ba1dafc95dcd6917ea3738
Ivan De Marino <ivan.demarino@snyk.io>
2026-09-03 20:25:20 +0100

    feat: setup of Jetbrains Toolbox scripts in PATH

---
+++ 40-jetbrains.sh
@@ -0,0 +1,4 @@
+if [[ -d "${HOME}/Library/Application Support/JetBrains/Toolbox/scripts" ]]; then
+	info "🤓 Setup PATH: Jetbrains Toolbox Scripts"
+	export PATH="${HOME}/Library/Application Support/JetBrains/Toolbox/scripts:${PATH}"
+fi
